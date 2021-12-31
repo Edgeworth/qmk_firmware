5926b28dbff13111e357df7dc25f5c7212ecc788
E <edgeworth.dev@gmail.com>
2021-12-31 15:57:25 +0900

    Add memekbd v2

---
+++ keyboards/memekbd/rules.mk
@@ -0,0 +1,12 @@
+
+
+# Enabled options
+RGBLIGHT_ENABLE = no        # For key backlights
+AUDIO_ENABLE = yes          # Audio output, for buzzer.
+
+# Disabled options
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+RGB_MATRIX_ENABLE = no
+TAP_DANCE_ENABLE = no
